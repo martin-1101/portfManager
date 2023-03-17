ae82a89982949f8eb358ebfc58bb7fbd51b82519
martin-1101 <martinrvn@protonmailcom>
2023-03-17 13:33:16 +0100

    tablewriter añadido

--- application/asset_service.go
+++ application/asset_service.go
@@ -3,9 +3,13 @@ package application
 import "github.com/martin-1101/portfManager/domain"
 
 type AssetService struct {
-	Repo domain.AssetRepository
+	repo domain.AssetRepository
 }
 
-func (as *AssetService) GetAssets() ([]domain.Asset, error) {
-	return as.Repo.GetAssets()
+func NewAssetService(repo domain.AssetRepository) *AssetService {
+	return &AssetService{repo: repo}
+}
+
+func (s *AssetService) GetAllAssets() ([]domain.Asset, error) {
+	return s.repo.GetAssets()
 }
